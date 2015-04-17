4c5664316f182cb6ac4db8c612ccb026f9e70c50
fayadh <fayadh.almosawi@gmail.com>
2015-04-16 20:13:14 -0400

    Dynamic routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Picture.create!(
+	:title => 'Bottle Caps.',
+	:artist => 'The Universe',
+	:url => 'http://www.hdwallpapersimages.com/wp-content/uploads/2014/01/Winter-Tiger-Wild-Cat-Images.jpg'
+)
+
+#Pictures is the MODEL i'm talking about. The DB talks to the MODEL. 
